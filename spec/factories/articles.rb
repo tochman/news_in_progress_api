ad3a2bff73182af36a76a16d4c20de966e76efcf
Dorian Buck <dorianbuck@gmail.com>
2021-10-13 23:14:42 +0200

    adds formating of code

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,9 +2,6 @@ FactoryBot.define do
   factory :article do
     title { 'MyTitle' }
     lede { 'MyLede' }
-    main_image { 'MainImage' }
-    url { 'url.com' }
-    # date { 2021-10-13 }
     body { 'MyBody' }
   end
 end
