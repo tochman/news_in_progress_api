8921512c9fe2671d7638378b450c24a940909ee2
Dorian Buck <dorianbuck@gmail.com>
2021-10-13 22:51:24 +0200

    adds article_spec tests and coverage

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,22 @@
-require 'rails_helper'
-
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table' do
+    it { is_expected.to have_db_column :title }
+    it { is_expected.to have_db_column :lede }
+    it { is_expected.to have_db_column :body }
+    it { is_expected.to have_db_column :url }
+    it { is_expected.to have_db_column :date }
+    it { is_expected.to have_db_column :main_image }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_presence_of :lede }
+    it { is_expected.to validate_presence_of :body }
+  end
+
+  describe 'Factory' do
+    it 'is expected to have valid Factory' do
+      expect(create(:article)).to be_valid
+    end
+  end
 end
