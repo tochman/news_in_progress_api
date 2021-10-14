8d094ac71179f49d4c8eb749eabc67e13679ca78
Fraser <Fraser.Hughes@btinternet.com>
2021-10-14 23:20:04 +0200

    Adds sad path for missing title and lede

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -11,6 +11,32 @@ class Api::ArticlesController < ApplicationController
   def create
     article = Article.create(title: params[:title],
                              lede: params[:lede])
-    render json: { message: "You have successfully added #{article.title} to the site" }, status: 201
+
+    # render json: { message: "You have successfully added #{article.title} to the site" }, status: 201
+
+    # validate!(article)
+
+    # begin
+    #   article.validate!
+    # rescue StandardError
+    #   render json: { message: 'Please add a title to your article' }
+    # else
+    #   render json: { message: "You have successfully added #{article.title} to the site" }, status: 201
+    # end
+
+    if article.title.nil?
+      render json: { message: 'Please add a title to your article' }
+    elsif article.lede.nil?
+      render json: { message: 'Please add a lede to your article' }
+    else
+      render json: { message: "You have successfully added #{article.title} to the site" }, status: 201
+    end
   end
+
+  private
+
+  # def validate!(article)
+  #   errors.add(:title, :blank, message: 'Please add a lede to your article') if article.title.nil?
+  #   errors.add(:lede, :blank, message: 'Please add a lede to your article') if article.lede.nil?
+  # end
 end
--- spec/requests/api/create_article_spec.rb
+++ spec/requests/api/create_article_spec.rb
@@ -1,18 +1,45 @@
 RSpec.describe 'POST /api/articles', type: :request do
   subject { response }
   let(:article) { create(:article, title: 'Big News!') }
-  describe '' do
+  describe 'when the article is successfully created' do
     before do
       post '/api/articles',
            params: { title: article.title,
                      lede: article.lede }
     end
+
     it { is_expected.to have_http_status 201 }
 
     it 'is expected to return a respomse message when a article is created' do
       expect(response_json['message']).to eq(
         "You have successfully added #{article.title} to the site"
       )
+    end
+  end
+
+  describe 'when the article is missing ' do
+    describe 'the title' do
+      before do
+        post '/api/articles'
+      end
+
+      it 'is expected to to ask for a title when the title is missing' do
+        expect(response_json['message']).to eq(
+          'Please add a title to your article'
+        )
+      end
+    end
+
+    describe 'the lede' do
+      before do
+        post '/api/articles',
+             params: { title: article.title}
+      end
+      it 'is expected to to ask for a lede when the lede is missing' do
+        expect(response_json['message']).to eq(
+          'Please add a lede to your article'
+        )
+      end
     end
   end
 end
