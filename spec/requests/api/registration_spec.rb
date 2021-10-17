63247fb7cdfc0cb9d7bb55e59e85d294a4304955
August <august.enhager@gmail.com>
2021-10-17 16:46:51 +0200

    adds sad path for user creation test and updated routes

--- spec/requests/api/registration_spec.rb
+++ spec/requests/api/registration_spec.rb
@@ -1,4 +1,5 @@
 RSpec.describe 'POST /api/auth/' do
+  subject { response }
   describe 'successful registration' do
     before do
       post '/api/auth/',
@@ -19,6 +20,23 @@ RSpec.describe 'POST /api/auth/' do
 
     it 'is expected to that users database contains new user' do
       expect(User.all.first.email).to eq 'user@email.com'
+    end
+  end
+
+  describe 'unsuccessful registration' do
+    before do
+      post '/api/auth/',
+           params: {
+             email: 'user@emial.com',
+             name: 'foobar',
+             password: 'passsword',
+             password_confirmation: 'wrong password',
+             confirmation_success_url: 'placeholder'
+           }
+    end
+
+    it 'is expected to return a error message when password do not match' do
+      expect(response_json['errors']['full_messages']).to eq ["Password confirmation doesn't match Password"]
     end
   end
 end
