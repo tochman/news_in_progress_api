71fef2f33f641b7198f57c8aca7838eb205ba1d3
Fraseer <fraseer1430@gmail.com>
2021-10-19 18:16:14 +0200

    Creates seed files

---
+++ db/seeds/001_categories.rb
@@ -0,0 +1,8 @@
+Category.find_or_create_by(name: "World News")
+Category.find_or_create_by(name: "Science & Enviroment")
+Category.find_or_create_by(name: "Business")
+Category.find_or_create_by(name: "Tech")
+Category.find_or_create_by(name: "Entertainment")
+Category.find_or_create_by(name: "Culture & Health")
+Category.find_or_create_by(name: "Sports")
+
