c9898a323c71e513a776ba0f2220d3983d2e4d72
Thomas <thomas@craftacademy.se>
2021-10-21 16:08:59 +0200

    cleans up the test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -59,14 +59,14 @@ RSpec.describe User, type: :model do
   end
 
   describe 'associations' do
+    it { is_expected.to have_and_belong_to_many(:articles) }
     describe 'has many :articles' do
-      subject { user_1 }
-      let(:user_1) { create(:journalist) }
+      subject { create(:journalist) }
       let(:user_2) { create(:journalist) }
-      let!(:article) { create(:article, authors: [user_1, user_2]) }
+      let!(:article) { create(:article, authors: [subject, user_2]) }
 
-      it do
-        expect(subject.articles.count).to eq 1
+      it 'is expected to include article in collection' do
+        expect(subject.articles).to include article
       end
     end
   end
