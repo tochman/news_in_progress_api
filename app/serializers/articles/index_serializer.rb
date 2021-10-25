175c97c1139e88675c46829c32852e55a4a1e161
Dorian Buck <dorianbuck@gmail.com>
2021-10-25 20:58:52 +0200

    adds test for serializer index

--- app/serializers/articles/index_serializer.rb
+++ app/serializers/articles/index_serializer.rb
@@ -1,5 +1,5 @@
 class Articles::IndexSerializer < ActiveModel::Serializer
-  attributes :id, :title, :lede, :created_at, :updated_at, :published
+  attributes :id, :title, :lede, :updated_at, :published
 
   belongs_to :category, serializer: Categories::IndexSerializer
   has_many :authors, serializer: Users::AuthorsIndexSerializer
