032a0691cde71a3a98dd82f935141f992a6f09f6
Fraseer <Fraser.Hughes@btinternet.com>
2021-10-26 19:57:23 +0200

    adds rescue from for different errors

--- app/controllers/api/subscriptions_controller.rb
+++ app/controllers/api/subscriptions_controller.rb
@@ -1,24 +1,20 @@
 class Api::SubscriptionsController < ApplicationController
-before_action :authenticate_user!, only: :create
+  before_action :authenticate_user!, only: :create
   rescue_from Stripe::CardError, with: :rescue
+  rescue_from Stripe::InvalidRequestError, with: :rescue
 
   def create
     customer = Stripe::Customer.list(email: current_user.email).data.first
     customer ||= Stripe::Customer.create(email: current_user.email, source: params[:stripeToken]) unless customer
-    payment = Stripe::Charge.create(customer: customer.id, currency: 'sek', amount: 50_000)
-    if payment[:paid]
-      render json: {message: 'You have successfully subscribed'}, status: 201
-      current_user.subscriber!
-    else
-      render json: {error: 'Something went wrong!'}, status: 422
-    end
+    Stripe::Charge.create(customer: customer.id, currency: 'sek', amount: 50_000)
 
+    render json: { message: 'You have successfully subscribed' }, status: 201
+    current_user.subscriber!
   end
 
   private
 
   def rescue(e)
-    binding.pry
-    render json: {error: e.message}, status: 402
+    render json: { errors: e.message }, status: e.http_status
   end
 end
--- spec/requests/api/subscription_create_spec.rb
+++ spec/requests/api/subscription_create_spec.rb
@@ -2,17 +2,16 @@ describe 'POST /api/subscriptions' do
   let(:stripe_helper) { StripeMock.create_test_helper }
   let(:valid_card_token) { stripe_helper.generate_card_token }
   let(:invalid_card_token) { '123456' }
+  let(:user) { create(:registered_user) }
+  let(:credentials) { user.create_new_auth_token }
 
   before(:each) do
-        StripeMock.start
+    StripeMock.start
   end
 
   after(:each) do
     StripeMock.stop
   end
-
-  let(:user) { create(:registered_user) }
-  let(:credentials) { user.create_new_auth_token }
 
   subject { response }
 
@@ -29,15 +28,46 @@ describe 'POST /api/subscriptions' do
     end
   end
 
-  describe 'when the card is declined' do
-    before do 
-      StripeMock.start
+  describe 'with an invalid cart token' do
+    before do
+      post '/api/subscriptions', params: { stripeToken: invalid_card_token }, headers: credentials
     end
-    StripeMock.prepare_card_error(:card_declined)
-    it { is_expected.to have_http_status 422 }
 
-    it 'is expected to return an error message when the card is declined' do
-      expect(response_json["error"]).to eq ""
+    it { is_expected.to have_http_status 404 }
+
+    it 'is expected to update the user to subscriber' do
+      user.reload
+      expect(user.subscriber?).to eq false
+    end
+
+    it 'is expected to return an error message' do
+      expect(response_json['errors']).to eq 'Invalid token id: 123456'
     end
   end
+  
+  describe 'when the card is declined' do
+    before do
+      StripeMock.prepare_card_error(:card_declined)
+      post '/api/subscriptions', params: { stripeToken: valid_card_token }, headers: credentials
+    end
+    
+    it { is_expected.to have_http_status 402 }
+
+    it 'is expected to return an error message' do
+      expect(response_json['errors']).to eq 'The card was declined'
+    end
+  end
+  describe 'when the card has a processing_error' do
+    before do
+      StripeMock.prepare_card_error(:processing_error)
+      post '/api/subscriptions', params: { stripeToken: valid_card_token }, headers: credentials
+    end
+
+    it { is_expected.to have_http_status 402 }
+
+    it 'is expected to return an error message' do
+      expect(response_json['errors']).to eq 'An error occurred while processing the card'
+    end
+  end
+  
 end
