6bc886ab4b96997fb1526d79db2405ef575daa3f
Fraseer <Fraser.Hughes@btinternet.com>
2021-10-27 17:00:44 +0200

    creates seed file for users

---
+++ db/seeds/002_users.rb
@@ -0,0 +1,3 @@
+User.find_or_create_by(name: 'Roger Harrabin', email: 'roger@nip.com', role: 2)
+User.find_or_create_by(name: 'Emily Anthes', email: 'emily@nip.com', role: 2)
+User.find_or_create_by(name: 'Becky Waterton', email: 'becky@nip.com', role: 2)
