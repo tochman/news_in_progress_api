fc4094855fe624f54ef89a489e3048672da0e3c9
maxarvid <max.arvid.anderson@gmail.com>
2021-11-01 13:41:22 +0100

    refactors articles seed using find_and_initialize

--- db/seeds/002_users.rb
+++ db/seeds/002_users.rb
@@ -2,3 +2,5 @@ User.create(name: 'Roger Harrabin', email: 'roger@nip.com', role: 2, password: '
 User.create(name: 'Emily Anthes', email: 'emily@nip.com', role: 2, password: 'password')
 User.create(name: 'Becky Waterton', email: 'becky@nip.com', role: 2, password: 'password')
 User.create(name: 'Pascale Hughes', email: 'pascale@nip.com', role: 2, password: 'password')
+User.create(name: 'Fraser Hughes', email: 'fraser@nip.com', role: 3, password: "password")
+User.create(name: 'Matthew Henry', email: 'matthew@nip.com', role: 2, password: "password")
